f795097eda24fbd01b6e8117a6b073ee3fd79cf9
umair-eritheia <umairr.murtaza@gmail.com>
2018-02-11 15:55:04 +0500

    Initial Setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: 'hotels' do
+	  resources :base
+	end
+
+	scope module: 'cars' do
+	  resources :base
+	end
+
+	scope module: 'flights' do
+	  resources :base
+	end
 end
